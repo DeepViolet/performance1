0272314c242c1feef6609257cda6dc983f29554d
DeepViolet <liuhuiyi1989@gmail.com>
2017-05-24 22:34:17 +0800

    引入synchronized 进行延迟加载

---
+++ src/SingtonTest.java
@@ -0,0 +1,42 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by liuhuiyi on 2017/5/24.
+ */
+public class SingtonTest extends Thread {
+    public static class AccessSingletonThread extends Thread {
+        long begintime;
+
+        public AccessSingletonThread(long begintime) {
+            this.begintime = begintime;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("try to get instance");
+            for (int i = 0; i < 100000; i++)
+                //Singleton.getInstance();
+                Singleton.getInstance();
+            System.out.println("spend:" + (System.currentTimeMillis() - begintime));
+        }
+    }
+
+    //@Test
+    public static void testPerformance() throws InterruptedException {
+        ExecutorService exe = Executors.newFixedThreadPool(5);
+        long begintime = System.currentTimeMillis();
+        exe.submit(new AccessSingletonThread(begintime));
+        exe.submit(new AccessSingletonThread(begintime));
+        exe.submit(new AccessSingletonThread(begintime));
+        exe.submit(new AccessSingletonThread(begintime));
+        exe.submit(new AccessSingletonThread(begintime));
+
+        Thread.sleep(10000);
+    }
+
+    public static void testSingleton() {
+        StaticSingleton.createString();
+       // StaticSingleton.getInstance();
+    }
+}
---
+++ src/StaticSingleton.java
@@ -0,0 +1,18 @@
+/**
+ * Created by liuhuiyi on 2017/5/24.
+ */
+public class StaticSingleton {
+    private StaticSingleton(){
+        System.out.println("Singleton is create");
+    }
+    private static class SingleTonHolder{
+        private static StaticSingleton instance = new StaticSingleton();
+    }
+    public static StaticSingleton getInstance()
+    {
+        return SingleTonHolder.instance;
+    }
+    public static void createString(){
+        System.out.println("create string in Singleton");
+    }
+}
